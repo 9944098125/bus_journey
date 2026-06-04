a2760a644ba63365b74fe051f01121ce0300ba76
Srinivas Akella <akellasrinivas9944@gmail.com>
2026-06-04 23:18:04 +0530

    deployed backend to aws

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -43,7 +43,7 @@ app.use(helmet());
 
 app.use(
 	cors({
-		origin: "*",
+		origin: process.env.FRONTEND_URL,
 		credentials: true,
 	}),
 );
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,9 +6,11 @@ const app = express();
 app.use("/health", healthRouter);
 
 app.get("/", (_, res) => {
-  res.send("API Running");
+	res.send("API Running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Server running on port ${PORT}`);
 });
